edde58cbf32f0cdb439e8285d53a3307f8f43bfd
JorgeMoragaCalvo <jorge.moraga.c@usach.cl>
2024-09-17 15:47:46 -0300

    sum list and generate list

--- utils-list.rkt
+++ utils-list.rkt
@@ -1,5 +1,8 @@
 #lang racket
 
+;(require "swap.rkt")
+(require "basic-operations.rkt")
+
 (provide major)
 (provide my-length)
 (provide minor)
@@ -19,3 +22,18 @@
                                          accu
                                          (length (cdr l) (+ accu 1)))))
                     (length lst 0)))
+
+; 
+(define sum-list (lambda (lst)
+                   (define sum (lambda (l accu)
+                                 (if (null? l)
+                                     accu
+                                     (sum (cdr l)(my-sum (car l) accu)))))
+                   (sum lst 0)))
+
+(define list-from-to (lambda (x)
+                       (define list-from (lambda (n1 n2 lst)
+                                           (if (>= n2 n1)
+                                               lst
+                                               (list-from n1 (my-sum n2 1) (cons n2 lst)))))
+                       (list-from x 1 '())))
